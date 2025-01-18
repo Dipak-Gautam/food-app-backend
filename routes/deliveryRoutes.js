b99b81bd9ed74a685d4e2491af1c0c57fcb92da4
Dipak-Gautam <anjangautam095@gmail.com>
2025-01-18 17:20:36 +0545

    feat:delivery/token manipulation

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Delivery = require("../modals/delivery");
+const User = require("../modals/users");
 
 router.post("/", async (req, res) => {
   try {
     const data = req.body;
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    data.deliveredBy = user.name;
     const newDelivery = new Delivery(data);
     await newDelivery.save();
     res.status(200).json("delivery saved sucessfully");
